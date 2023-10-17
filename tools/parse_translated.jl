e2ae21ba5414720730bd72315caed2f84306a7c5
stdblue <signe.stdblue@gmail.com>
2023-10-17 22:17:31 +0900

    JASCOM 翻訳文をマージしたリソースを作成。

---
+++ tools/parse_translated.jl
@@ -0,0 +1,31 @@
+###
+
+using Printf
+
+function filter_translated(filename)
+    heads = Dict{String, String}();
+    ini_io = open(filename, "r")
+
+    if isopen(ini_io) == false
+        println("Failed to open " * target_file)
+        exit()
+    end
+
+    for buff in eachline(ini_io)
+        keywords = split(buff, "\t")
+        if length(keywords) > 2
+            cnt = length(keywords[3])
+            if cnt > 0
+                println(keywords[1] * "=" * keywords[2])
+                heads[keywords[1]] = keywords[2]
+            end
+        end
+    end
+    close(ini_io)
+
+    return heads
+end
+
+dicts = filter_translated(ARGS[1])
+
+exit
