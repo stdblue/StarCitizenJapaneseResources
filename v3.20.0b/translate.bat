f1563c9bd21f78a6ee4bfff96ab2c0e28f02d5b0
stdblue <signe.stdblue@gmail.com>
2023-10-18 22:24:21 +0900

    Alpha 1.3 リリース準備

--- v3.20.0b/translate.bat
+++ v3.20.0b/translate.bat
@@ -10,11 +10,8 @@ REM mobiGlass のコンタクトマネージャーを開くと落ちる部分を
 julia ..\..\tools\split_replace.jl global.ini.machine ..\library\manual\momo_1_translated.txt >global.ini.momo
 
 REM JASCOM とりまとめワークシートを適用
-julia ..\..\tools\split_replace.jl global.ini.momo ..\library\manual\jascom_1_translated.txt >global.ini.jascom
-
-REM mo55y さん翻訳分を適用
-julia ..\..\tools\split_replace.jl global.ini.jascom ..\library\manual\mossy_1_message.txt ..\library\manual\mossy_2_message.txt ..\library\manual\mossy_3_message.txt>global.ini.mossy
+julia ..\..\tools\split_replace.jl global.ini.momo ..\library\manual\jascom_translated.txt >global.ini.jascom
 
 REM ぴにきさん翻訳分を適用
-julia ..\..\tools\split_replace.jl global.ini.mossy ..\library\manual\mossy_1_message.txt ..\library\manual\mossy_2_message.txt ..\library\manual\mossy_3_message.txt>global.ini
+julia ..\..\tools\split_replace.jl global.ini.jascom ..\library\manual\mossy_1_message.txt ..\library\manual\mossy_2_message.txt ..\library\manual\mossy_3_message.txt>global.ini
 
