894b924b473f16f5f05054c8281bd6a9e810c328
stdblue <signe.stdblue@gmail.com>
2023-10-18 12:16:13 +0900

    alpha.1.2 準備

--- v3.20.0b/translate.bat
+++ v3.20.0b/translate.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM これは翻訳をまとめてマージするためのコマンドです。
-REM work フォルダーにて実行してください。
+cd work
 
 REM v3.16.1 の機械翻訳を適用（ただし、変数を除く）
 julia ..\..\tools\split_headers.jl ..\global_en.ini ..\library\machine\global_ja_00 ..\library\machine\global_ja_01 ..\library\machine\global_ja_02 ..\library\machine\global_ja_03 ..\library\machine\global_ja_04 ..\library\machine\global_ja_05 ..\library\machine\global_ja_06 >global.ini.machine
@@ -13,6 +13,8 @@ REM JASCOM とりまとめワークシートを適用
 julia ..\..\tools\split_replace.jl global.ini.momo ..\library\manual\jascom_1_translated.txt >global.ini.jascom
 
 REM mo55y さん翻訳分を適用
-julia ..\..\tools\split_replace.jl global.ini.jascom ..\library\manual\mossy_1_message.txt ..\library\manual\mossy_2_message.txt >global.ini
+julia ..\..\tools\split_replace.jl global.ini.jascom ..\library\manual\mossy_1_message.txt ..\library\manual\mossy_2_message.txt ..\library\manual\mossy_3_message.txt>global.ini.mossy
 
+REM ぴにきさん翻訳分を適用
+julia ..\..\tools\split_replace.jl global.ini.mossy ..\library\manual\mossy_1_message.txt ..\library\manual\mossy_2_message.txt ..\library\manual\mossy_3_message.txt>global.ini
 
