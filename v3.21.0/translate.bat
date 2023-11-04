7db174148da9bc7429da76c926d214cb5a946a02
stdblue <signe.stdblue@gmail.com>
2023-11-04 12:48:09 +0900

    現状のリリース作成手順に更新

--- v3.21.0/translate.bat
+++ v3.21.0/translate.bat
@@ -10,6 +10,7 @@ REM SCJPベースを取り込み。
 julia ..\..\tools\split_headers.jl global.v3.20.ini ..\library\manual\scjpbase.txt >global.base.ini
 
 REM v3.21 差分を取り込み。
-julia ..\..\tools\split_headers.jl global.base.ini ..\library\manual\scjpv321.txt >global.ini
+julia ..\..\tools\split_headers.jl global.base.ini ..\library\manual\scjpv321.txt >global.85299.ini
 
-
+REM 3DTEXT 障害(STARC-85299)回避のリソース上書き
+julia ..\..\tools\split_headers.jl global.85299.ini global.3d.txt >global.ini
