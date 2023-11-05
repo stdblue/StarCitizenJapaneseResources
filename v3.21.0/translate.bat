50fc8df17ae7654f25366243446a5ed40671171a
stdblue <signe.stdblue@gmail.com>
2023-11-05 18:43:45 +0900

    3DTEXT追加対応。Alpha 2.3 リリース。

--- v3.21.0/translate.bat
+++ v3.21.0/translate.bat
@@ -1,23 +1,28 @@
 @echo off
-setlocal
 
+REM 翻訳をまとめてマージするためのコマンド
+
+setlocal
 set verdir="%~dp0"
 
-REM これは翻訳をまとめてマージするためのコマンドです。
+echo %verdir%
+
 REM 作業ディレクトリへ移動
-pushd %verdir%\work
+pushd "%verdir%\work"
+
+echo Work directory : %CD%
 
 REM v3.20.0b の日本語リソースをベースとする。
-julia %verdir%\..\tools\split_headers.jl ..\global.ini ..\..\v3.20.0b\release\global.ini >global.v3.20.ini
+julia ..\..\tools\split_headers.jl ..\global.ini ..\..\v3.20.0b\release\global.ini >global.v3.20.ini
 
 REM SCJPベースを取り込み。
-julia %verdir%\..\tools\split_headers.jl global.v3.20.ini ..\library\manual\scjpbase.txt >global.base.ini
+julia ..\..\tools\split_headers.jl global.v3.20.ini ..\library\manual\scjpbase.txt >global.base.ini
 
 REM v3.21 差分を取り込み。
-julia %verdir%\..\tools\split_headers.jl global.base.ini ..\library\manual\scjpv321.txt >global.85299.ini
+julia ..\..\tools\split_headers.jl global.base.ini ..\library\manual\scjpv321.txt >global.85299.ini
 
 REM 3DTEXT 障害(STARC-85299)回避のリソース上書き
-julia %verdir%\..\tools\split_headers.jl global.85299.ini ..\global.3d.txt >global.ini
+julia ..\..\tools\split_headers.jl global.85299.ini ..\global.3d.txt >global.ini
 
 popd
 
