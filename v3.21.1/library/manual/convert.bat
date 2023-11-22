d53c082dda114db0ff5c94b270e3c1959a0be1cc
stdblue <signe.stdblue@gmail.com>
2023-11-22 21:44:23 +0900

    Alpha 3.1 リリース

---
+++ v3.21.1/library/manual/convert.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+
+set batchdir="%~dp0"
+
+set target="20231122"
+
+julia ..\..\..\tools\parse_translated.jl base_%target%.tsv > scjpbase.txt
+
+for /F "tokens=2,3" %%I in ('where /R . /T "scjpbase.txt"') do set STAMP=%%I-%%J
+
+echo scjpbase.txt %STAMP%
+
+julia ..\..\..\tools\parse_translated.jl v321_%target%.tsv > scjp321.txt
+
+for /F "tokens=2,3" %%I in ('where /R . /T "scjp321.txt"') do set STAMP=%%I-%%J
+
+echo scjp321.txt %STAMP%
+
+julia ..\..\..\tools\parse_translated.jl v3211_%target%.tsv > scjp3211.txt
+
+for /F "tokens=2,3" %%I in ('where /R . /T "scjp3211.txt"') do set STAMP=%%I-%%J
+
+echo scjp3211.txt %STAMP%
+
+
