c9626764b68c525d3803f545ff958e138d66f791
stdblue <signe.stdblue@gmail.com>
2023-12-07 12:42:09 +0900

    Tool 改修予定

---
+++ tools/merge_globals/src/main.rs
@@ -0,0 +1,20 @@
+//! Star Citizens localization resource merger
+//!
+//! This module merges translated resources in order left to right.
+//! You can overwrite old translated resouce with new ones.
+//!
+//! # Usage
+//!
+//! ```
+//!   merge_global global.ini [translated global.ini ...]
+//!
+//!  Options:
+//!      global.ini : newest english resource
+//!      translated global.ini : translated resource same with global.ini format
+//!
+//! ```
+
+/// Startup
+fn main() {
+    println!("Hello, world!");
+}
--- v3.22.0/translate.bat
+++ v3.22.0/translate.bat
@@ -13,14 +13,14 @@ pushd "%verdir%\work"
 echo Work directory : %CD%
 
 REM v3.20.0b の日本語リソースをベースとする。
-julia ..\..\tools\split_headers.jl ..\global.ini ..\..\v3.21.0\release\global.ini >global.v3.21.ini
+julia ..\..\tools\split_headers.jl ..\global.ini ..\..\v3.21.1\release\global.ini >global.v3211.ini
 
 REM SCJPベースを取り込み。
-julia ..\..\tools\split_replace.jl global.v3.21.ini ..\library\manual\scjpbase.txt ..\library\manual\scjp321.txt ..\library\manual\scjp3211.txt>global.base.ini
+REM julia ..\..\tools\split_replace.jl global.v3.21.ini ..\library\manual\scjpbase.txt ..\library\manual\scjp321.txt ..\library\manual\scjp3211.txt>global.base.ini
 REM julia ..\..\tools\split_replace.jl ..\global.ini ..\library\manual\scjpbase.txt ..\library\manual\scjp321.txt ..\library\manual\scjp3211.txt>global.base.ini
 
 REM 3DTEXT 障害(STARC-85299)回避のリソース上書き
-julia ..\..\tools\split_replace.jl global.base.ini ..\global.3d.txt >global.ini
+julia ..\..\tools\split_replace.jl global.v3211.ini ..\global.3d.txt >global.ini
 
 popd
 
