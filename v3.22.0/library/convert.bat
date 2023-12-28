9e8e1e4a33d52db53a1ede52c870af764ad0cb3b
stdblue <signe.stdblue@gmail.com>
2023-12-28 23:50:23 +0900

    LIVE 9003408 版

--- v3.22.0/library/convert.bat
+++ v3.22.0/library/convert.bat
@@ -3,27 +3,27 @@ setlocal
 
 set batchdir="%~dp0"
 
-set target="20231211"
+set target="20231228"
 
-julia ..\..\tools\parse_translated.jl base_%target%.tsv > scjpbase.txt
+julia ..\..\tools\parse_translated.jl %target%\base.tsv > scjpbase.txt
 
 for /F "tokens=2,3" %%I in ('where /R . /T "scjpbase.txt"') do set STAMP=%%I-%%J
 
 echo scjpbase.txt %STAMP%
 
-julia ..\..\tools\parse_translated.jl v321_%target%.tsv > scjp321.txt
+julia ..\..\tools\parse_translated.jl %target%\v321.tsv > scjp321.txt
 
 for /F "tokens=2,3" %%I in ('where /R . /T "scjp321.txt"') do set STAMP=%%I-%%J
 
 echo scjp321.txt %STAMP%
 
-julia ..\..\tools\parse_translated.jl v3211_%target%.tsv > scjp3211.txt
+julia ..\..\tools\parse_translated.jl %target%\v3211.tsv > scjp3211.txt
 
 for /F "tokens=2,3" %%I in ('where /R . /T "scjp3211.txt"') do set STAMP=%%I-%%J
 
 echo scjp3211.txt %STAMP%
 
-julia ..\..\tools\parse_translated.jl v322_%target%.tsv > scjp322.txt
+julia ..\..\tools\parse_translated.jl %target%\v322.tsv > scjp322.txt
 
 for /F "tokens=2,3" %%I in ('where /R . /T "scjp3211.txt"') do set STAMP=%%I-%%J
 
