d62a925f289515713be1d6292639b6a4a6e555d6
stdblue <signe.stdblue@gmail.com>
2024-05-21 23:45:21 +0900

    v3.23.1 LIVE 対応

---
+++ v3.23.1/translate.bat
@@ -0,0 +1,20 @@
+@echo off
+
+REM 翻訳をまとめてマージするためのコマンド
+
+setlocal
+set verdir="%~dp0"
+
+echo %verdir%
+
+REM 作業ディレクトリへ移動
+pushd "%verdir%\work"
+
+echo Work directory : %CD%
+
+REM v3.23.0 の日本語リソースを取り込み。
+julia ..\..\tools\split_headers.jl ..\source\english\global.ini ..\library\20240520\global.ini >global.ini
+
+popd
+
+pause
