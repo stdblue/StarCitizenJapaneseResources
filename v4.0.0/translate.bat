feac82bc736f96a860ab200942abf48f76fe09cc
stdblue <signe.stdblue@gmail.com>
2025-03-14 09:23:08 +0900

    v4.1.0 PTU 9622512 対応。

--- v4.0.0/translate.bat
+++ v4.0.0/translate.bat
@@ -13,7 +13,7 @@ pushd "%verdir%\work"
 echo Work directory : %CD%
 
 REM v4.0.0 の日本語リソースを取り込み。
-julia ..\..\tools\split_headers.jl ..\source\english\global.ini ..\library\20250301\global.ini >global.ini
+julia ..\..\tools\split_headers.jl ..\source\english\global.ini ..\library\20250313\global.ini >global.ini
 
 popd
 
